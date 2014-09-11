85627a4915ec122c3ce2b3b14017d8534cd00188
dilumaflorence.d <dilumaflorence.d@SISBLRDKT050.sigmainfo.com>
2014-09-11 19:40:23 +0530

    asdonline and sports tailgate

--- src/main/java/org/jsoup/examples/OutdoorRetailer.java
+++ src/main/java/org/jsoup/examples/OutdoorRetailer.java
@@ -15,30 +15,30 @@ import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
-public class ASDOnline {
+public class OutdoorRetailer {
 
 	public static void main(String[] args) throws IOException,
 			InterruptedException {
 		long startTime = System.currentTimeMillis();
-		FileWriter writer = new FileWriter("/TOC/supplier_ASDOnline.csv");
+		FileWriter writer = new FileWriter("/TOC/supplier_OutDoorRetailer.csv");
 		String[] headers = { "Show", "Show Date", "Vendor name", "Booth No.",
-				"Website", "Address", "Phone", "Email", "Product Categories" };
+				"Website", "Address", "Email", "Phone" };
 		for (String header : headers) {
 			writer.append(header);
 			writer.append(',');
 		}
 		writer.append('\n');
 
-		String exhibitorUrl = "http://n1b.goexposoftware.com/events/asda14/goExpo/public/listExhibitorsFrame.php";
+		String exhibitorUrl = "http://n2b.goexposoftware.com/events/ors2014/goExpo/public/listExhibitorsFrame.php";
 		Document Showdoc = Jsoup.connect(exhibitorUrl).get();
 		Elements links = Showdoc.select("a[href*=id]");
 		int i = 0;
 		for (Element link : links) {
 			i++;
-			writer.append("LAS VEGAS");
+			writer.append("OUTDOOR RETAILER");
 			writer.append(",");
 
-			writer.append("August 3-6,2014");
+			writer.append("August 6-9 2014");
 			writer.append(",");
 
 			// Vendor Name
@@ -59,9 +59,10 @@ public class ASDOnline {
 			// Booth No.
 			int m = 1;
 			boolean flag = false;
+			String boothNumber = "";
 			while (m < 7) {
 				List<?> boothNoList = page
-						.getByXPath("/html/body/div[2]/div[1]/div[4]/div[2]/table/tbody/tr/td[1]/table[1]/tbody/tr/td[1]/div[2]/div[2]/a");
+						.getByXPath("/html/body/div[2]/div[1]/div[4]/div[4]/table/tbody/tr/td[1]/table[1]/tbody/tr/td[1]/div[2]/div[2]/a");
 				Iterator<?> boothNoDetail = boothNoList.iterator();
 				if (!boothNoDetail.hasNext()) {
 					Thread.sleep(1000);
@@ -69,10 +70,8 @@ public class ASDOnline {
 				}
 				while (boothNoDetail.hasNext()) {
 					HtmlElement boothNo = (HtmlElement) boothNoDetail.next();
-					String boothNumber = StringUtils.replace(boothNo.asText(),
-							",", " ");
-					writer.append(boothNumber);
-					writer.append(",");
+					boothNumber = StringUtils.replace(boothNo.asText(), ",",
+							" ");
 					flag = true;
 				}
 				if (flag) {
@@ -80,26 +79,38 @@ public class ASDOnline {
 				}
 			}
 
+			writer.append(boothNumber);
+			writer.append(",");
+
 			// Address
 			String websiteValue = "";
 			String addresDetail = "";
+			String emailValue = "";
 			List<?> addressList = page
-					.getByXPath("/html/body/div[2]/div[1]/div[4]/div[2]/table/tbody/tr/td[1]/table[1]/tbody/tr/td[2]/table");
+					.getByXPath("/html/body/div[2]/div[1]/div[4]/div[4]/table/tbody/tr/td[1]/table[1]/tbody/tr/td[2]/table");
 			Iterator<?> addressDetail = addressList.iterator();
 			while (addressDetail.hasNext()) {
 				HtmlElement address = (HtmlElement) addressDetail.next();
 				String addressValue = StringUtils.replace(address.asText(),
 						",", " ");
 				addressValue = StringUtils.remove(addressValue, "\r");
-				addressValue = StringUtils.remove(addressValue, "Website: 	");
+				addressValue = StringUtils.replace(addressValue, "Website: 	",
+						"\t");
+				addressValue = StringUtils.replace(addressValue, "Email: 	",
+						"\t");
+				addressValue = StringUtils.replace(addressValue, "Address: 	",
+						"\t");
 				addressValue = StringUtils.remove(addressValue, "\n");
-				String[] split = addressValue.split("Address: 	");
-				for (int l = 0; l < split.length; l++) {
+				String[] split = addressValue.split("\t");
+				for (int l = 1; l < split.length - 1; l++) {
 					String addressInfo = StringUtils
 							.replace(split[l], ",", " ");
 					if (addressInfo.contains("www")) {
 						websiteValue = addressInfo;
 						System.out.println(websiteValue);
+					} else if (addressInfo.contains("@")) {
+						emailValue = StringUtils.replace(addressInfo, ",", " ");
+						System.out.println(emailValue);
 					} else {
 						addresDetail = StringUtils.replace(addressInfo, "\n",
 								" ");
@@ -107,6 +118,18 @@ public class ASDOnline {
 					}
 				}
 
+			}
+			String phoneValue = "";
+			List<?> phoneList = page
+					.getByXPath("/html/body/div[2]/div[1]/div[4]/div[4]/table/tbody/tr/td[2]/table/tbody/tr/td/div");
+			Iterator<?> phoneDetail = phoneList.iterator();
+			while (phoneDetail.hasNext()) {
+				HtmlElement next = (HtmlElement) phoneDetail.next();
+				phoneValue = StringUtils.replace(next.asText(), ",", " ");
+				if (phoneValue.contains("@")) {
+					emailValue += " " + phoneValue;
+					phoneValue = "";
+				}
 			}
 			// Website
 			writer.append(websiteValue);
@@ -116,58 +139,13 @@ public class ASDOnline {
 			writer.append(addresDetail);
 			writer.append(",");
 
-			String emailValue = "";
-			String phoneValue = "";
-			List<?> contactList = page
-					.getByXPath("/html/body/div[2]/div[1]/div[4]/div[2]/table/tbody/tr/td[2]/table/tbody/tr/td");
-			Iterator<?> contactDetail = contactList.iterator();
-			while (contactDetail.hasNext()) {
-				HtmlElement next = (HtmlElement) contactDetail.next();
-				String contact = next.asText();
-				contact = StringUtils.remove(contact, "\r");
-				String[] split = contact.split("\n");
-				for (int l = 0; l < split.length; l++) {
-					String contactValue = StringUtils.replace(split[l], ",",
-							" ");
-					if (contactValue.contains("@")) {
-						emailValue = contactValue;
-					} else {
-						phoneValue = contactValue;
-					}
-				}
-			}
-
-			// Phone
-			writer.append(phoneValue);
-			writer.append(",");
-
 			// Email
 			writer.append(emailValue);
 			writer.append(",");
 
-			// Category
-			int count = 0;
-			List<?> categoryList = page
-					.getByXPath("/html/body/div[2]/div[1]/div[4]/div[2]/table/tbody/tr/td[1]/table[3]/tbody/tr/td[2]/div/div[5]");
-			Iterator<?> categoryDetail = categoryList.iterator();
-			while (categoryDetail.hasNext()) {
-				count++;
-				HtmlElement next = (HtmlElement) categoryDetail.next();
-				String categoryDetails = "\"";
-				String category = next.asText();
-				category = StringUtils.remove(category, "\r");
-				String categoryValue = StringUtils.replace(category, ",", " ");
-				categoryValue = StringUtils.remove(categoryValue,
-						"Product Categories:\n");
-				categoryDetails += categoryValue + "\"";
-				writer.append(categoryDetails);
-			}
-
-			// Category
-			if (count == 0) {
-				writer.append("");
-				writer.append(",");
-			}
+			// Phone
+			writer.append(phoneValue);
+			writer.append(",");
 
 			System.out.println("count" + i);
 			writer.append("\n");
@@ -178,4 +156,5 @@ public class ASDOnline {
 		System.out.println("Total time taken to prepare csv "
 				+ (endTime - startTime) / 1000 + " Seconds ");
 	}
+
 }
